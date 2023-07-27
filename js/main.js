77ef0e354f64c3964550ffe5fc29fa130b169f8a
jones <selvajones150@gmail.com>
2023-07-27 15:18:20 +0530

    filter-method

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var a=[1,2,3,4,5,6,7,8,9];
+document.write("filter method"+"<br>"+"<br>"+"a=[1,2,3,4,5,6,7,8,9]"+"<br>"+"Even number"+"<br>");
+
+var output=a.filter(function(e){
+	return e%2==0;
+})
+document.write(output);
